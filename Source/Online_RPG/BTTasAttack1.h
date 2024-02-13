4e9b4b8fd710a5e786b5c25ff253cc8081f7c44a
ycm23 <68059564+Yuchangminn1@users.noreply.github.com>
2024-02-13 21:14:57 +0900

    FixFix

--- Source/Online_RPG/BTTasAttack1.h
+++ Source/Online_RPG/BTTasAttack1.h
@@ -1,20 +1,20 @@
-//// Fill out your copyright notice in the Description page of Project Settings.
-//
-//#pragma once
-//
-//#include "CoreMinimal.h"
-////#include "BehaviorTree/BTTaskNode.h"
-//#include "BTTasAttack1.generated.h"
-//
-///**
-// * 
-// */
-//UCLASS()
-//class ONLINE_RPG_API UBTTasAttack1 : public UBTTaskNode
-//{
-//	GENERATED_BODY()
-//	UBTTasAttack1();
-//	//virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
-//
-//
-//};
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTasAttack1.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ONLINE_RPG_API UBTTasAttack1 : public UBTTaskNode
+{
+	GENERATED_BODY()
+	UBTTasAttack1();
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+
+};
