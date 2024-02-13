e395b3faead20f35a2da03eea5bcfe63011d7655
ycm23 <68059564+Yuchangminn1@users.noreply.github.com>
2024-02-13 21:08:36 +0900

    1

--- Source/Online_RPG/BTTasAttack1.cpp
+++ Source/Online_RPG/BTTasAttack1.cpp
@@ -1,25 +1,25 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "BTTasAttack1.h"
-#include "EnemyDog.h"
-#include "AIController.h"
-
-UBTTasAttack1::UBTTasAttack1()
-{
-	NodeName = "Attack";
-}
-
-
-EBTNodeResult::Type UBTTasAttack1::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
-{
-	Super::ExecuteTask(OwnerComp, NodeMemory);
-	if (OwnerComp.GetAIOwner() != nullptr)
-	{
-		AEnemyDog* QQ = Cast<AEnemyDog>(OwnerComp.GetAIOwner()->GetPawn());
-		QQ->Attack();
-	}
-	//OwnerComp.GetBlackboardComponent()->ClearValue(TEXT("TargetLastLocation"));
-
-	return EBTNodeResult::Succeeded;
-}
+//// Fill out your copyright notice in the Description page of Project Settings.
+//
+//
+//#include "BTTasAttack1.h"
+//#include "EnemyDog.h"
+//#include "AIController.h"
+//
+//UBTTasAttack1::UBTTasAttack1()
+//{
+//	NodeName = "Attack";
+//}
+//
+//
+////EBTNodeResult::Type UBTTasAttack1::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+////{
+////	Super::ExecuteTask(OwnerComp, NodeMemory);
+////	if (OwnerComp.GetAIOwner() != nullptr)
+////	{
+////		AEnemyDog* QQ = Cast<AEnemyDog>(OwnerComp.GetAIOwner()->GetPawn());
+////		QQ->Attack();
+////	}
+////	//OwnerComp.GetBlackboardComponent()->ClearValue(TEXT("TargetLastLocation"));
+////
+////	return EBTNodeResult::Succeeded;
+////}
