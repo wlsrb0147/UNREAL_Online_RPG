95624205947d82354db398687113bb62aa24ffae
akffoddl5 <akffoddl5@naver.com>
2024-02-13 20:46:35 +0900

    network_manager

--- Source/Online_RPG/WIdget_Login.cpp
+++ Source/Online_RPG/WIdget_Login.cpp
@@ -3,12 +3,19 @@
 
 #include "WIdget_Login.h"
 
+#include "Network_Manager_R.h"
+
 void UWIdget_Login::JoinClick()
 {
 	UE_LOG(LogTemp, Log, TEXT("Join CLick!!!!"));
+
+	
 }
 
 void UWIdget_Login::LoginClick()
 {
 	UE_LOG(LogTemp, Log, TEXT("Login CLick!!!!"));
+	UNetwork_Manager_R* Instance = Cast<UNetwork_Manager_R>(GetGameInstance());
+	
+	
 }
